f7f82d41531a3cd1925f61122449f5c07bb4c8bd
Konrad <dzumanet@gmail.com>
2024-10-11 11:38:16 +0200

    Added data refreshing to mutations.

--- src/queries/useCreatePlayerMutation.ts
+++ src/queries/useCreatePlayerMutation.ts
@@ -1,16 +1,21 @@
 import { useApi } from "../hooks/useApi.ts";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { PlayerDto, PlayerEntity } from "../types";
 
 export const useCreatePlayerMutation = () => {
     const { apiPost } = useApi();
+    const queryClient = useQueryClient();
 
     const { mutate, isPending } = useMutation({
         mutationKey: ['players', 'createPlayer'],
         mutationFn: async (payload: PlayerDto) => {
             return apiPost<PlayerEntity, PlayerDto>('players', payload);
         },
-
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['players']
+            });
+        }
     });
 
     return {
