ffef0361cb1e38c25b56649d11d4eb37fb9f66ad
Konrad <dzumanet@gmail.com>
2024-10-12 18:40:20 +0200

    feat: add PlayerDeleteConfirmation component for confirming player deletion

--- src/Players/DeletePlayerConfirmation.tsx
+++ src/Players/DeletePlayerConfirmation.tsx
@@ -1,9 +1,38 @@
-export const DeletePlayerConfirmation = () => {
+import { PlayerEntity } from "../types";
+import { useDeletePlayerMutation } from "../queries/useDeletePlayerMutation.ts";
+import { validatePlayerAssignedToTeam } from "../utils/validatePlayer.ts";
 
+type DeletePlayerConfirmationProps = {
+    onCancel: () => void;
+    player: PlayerEntity;
+}
+
+export const DeletePlayerConfirmation = ({ onCancel, player }: DeletePlayerConfirmationProps) => {
+    const { mutate, isPending } = useDeletePlayerMutation(player.id);
+
+    const validationError = validatePlayerAssignedToTeam(player);
+
+    const handleDelete = () => {
+        if (!validationError) {
+            mutate();
+        }
+    };
+
+    if (isPending) return <p>Loading...</p>;
 
     return (
         <div>
-            <p>Do you really want to delete player?</p>
+            {validationError ? (
+                <p>Player is assigned to a team. Cannot be removed.</p>
+            ) : (
+                <div>
+                    <p>Do you really want to delete player? <strong>{player.firstName} {player.lastName}</strong></p>
+                    <button onClick={handleDelete}>Delete</button>
+                    <button onClick={onCancel}>Cancel</button>
+                </div>)
+            }
+
         </div>
     )
-}
+        ;
+};
