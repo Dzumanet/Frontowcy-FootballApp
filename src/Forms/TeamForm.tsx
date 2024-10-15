d5a1e9edec2c4bc411542b66da9f438a7150b04c
Konrad <dzumanet@gmail.com>
2024-10-15 10:56:18 +0200

    refactor: extract player addition logic from AddTeam and TeamForm into a separate component AddPlayerToTeam  feat: add saving date as a numeric value in AddTeam

--- src/Forms/TeamForm.tsx
+++ src/Forms/TeamForm.tsx
@@ -1,16 +1,10 @@
 import { ChangeEvent, FormEvent, useState } from "react";
-import { PlayerEntity, TeamDto, TeamValidationErrors } from "../types";
+import { TeamDto, TeamValidationErrors } from "../types";
 import { validateTeam } from "../utils/validateTeam.ts";
 
 type TeamFormProps = {
     handleSubmit: (e: FormEvent) => void;
-    handleSelectChange: (e: ChangeEvent<HTMLSelectElement>) => void;
     handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
-    handleAddPlayer: () => void;
-    selectedPlayerId: string;
-    addedPlayers: PlayerEntity[]; // Lista graczy już dodanych do drużyny
-    availablePlayers: PlayerEntity[] | undefined; // Lista dostępnych graczy
-
     values: TeamDto;
     isPending: boolean;
     existingTeam?: TeamDto[];
@@ -18,11 +12,6 @@ type TeamFormProps = {
 
 export const TeamForm = ({
                              handleSubmit,
-                             handleAddPlayer,
-                             availablePlayers,
-                             selectedPlayerId,
-                             handleSelectChange,
-                             addedPlayers,
                              handleChange,
                              values,
                              isPending,
@@ -83,38 +72,7 @@ export const TeamForm = ({
                 onChange={handleChange}
                 required
             />
-
-            {/* Lista rozwijana do wyboru gracza */}
-            <label htmlFor="teamPlayers">Select Player to Add</label>
-            <select
-                name="teamPlayers"
-                id="teamPlayers"
-                value={selectedPlayerId}
-                onChange={handleSelectChange}
-            >
-                <option value="" disabled>Select player</option>
-                {availablePlayers
-                    ?.filter(player => !player.teamId)
-                    .map(player => (
-                    <option key={player.id} value={player.id}>
-                        {player.firstName}
-                    </option>
-                ))}
-            </select>
-            <button type="button" onClick={handleAddPlayer}>Add Player</button>
-
-
-            <h3>The Players will be added to the team:</h3>
-            <ul>
-                {addedPlayers.map(player => (
-                    <li key={player.id}>
-                        {player.firstName} {player.lastName}
-                    </li>
-                ))}
-            </ul>
-
             <button type="submit" disabled={isPending}>Save Team</button>
         </form>
-
     );
 };
--- src/Teams/AddTeam.tsx
+++ src/Teams/AddTeam.tsx
@@ -5,12 +5,13 @@ import { useCreateTeamMutation } from "../queries/useCreateTeamMutation.ts";
 import { useGetPlayersQuery } from "../queries/useGetPlayersQuery.ts";
 import { useUpdateMultiplePlayersTeamMutation } from "../queries/useUpdateMultiplePlayersTeamMutation.ts";
 import { useGetTeamsQuery } from "../queries/useGetTeamsQuery.ts";
+import { AddPlayersToTeam } from "./AddPlayersToTeam.tsx";
 
 export const AddTeam = () => {
     const { mutate: createTeam, isPending } = useCreateTeamMutation();
     const { mutate: updatePlayersTeam } = useUpdateMultiplePlayersTeamMutation();
     const { data: players } = useGetPlayersQuery();
-    const { data: team } = useGetTeamsQuery();
+    const { data: teams } = useGetTeamsQuery();
 
     const [selectedPlayerId, setSelectedPlayerId] = useState<string>('');
     const [addedPlayers, setAddedPlayers] = useState<PlayerEntity[]>([]);
@@ -51,28 +52,21 @@ export const AddTeam = () => {
         });
     };
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+        const { name, value, type } = e.target;
         setValues(prevValues => ({
             ...prevValues,
-            [name]: value
+            [name]: type === 'number' ? Number(value) : value,
         }));
     };
 
     return (
         <div>
             <h2>Add New Team</h2>
-            <TeamForm
-                handleSubmit={handleSubmit}
-                handleChange={handleChange}
-                handleSelectChange={handleSelectChange}
-                handleAddPlayer={handleAddPlayer}
-                selectedPlayerId={selectedPlayerId}
-                values={values}
-                addedPlayers={addedPlayers}
-                isPending={isPending}
-                availablePlayers={players}
-                existingTeam={team}
-            />
+            <TeamForm handleSubmit={handleSubmit} handleChange={handleChange} values={values} isPending={isPending}
+                      existingTeam={teams}/>
+            <AddPlayersToTeam availablePlayers={players || []} addedPlayers={addedPlayers}
+                              handleAddPlayer={handleAddPlayer} handleSelectChange={handleSelectChange}
+                              selectedPlayerId={selectedPlayerId}/>
         </div>
     );
 };
