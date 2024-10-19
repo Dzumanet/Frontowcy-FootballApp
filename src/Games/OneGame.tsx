4c4e8a000cf57701e33f39f350e1b237a9f7de42
Konrad <dzumanet@gmail.com>
2024-10-19 17:22:29 +0200

    refactor: display team name instead of team id in OneGame component.

--- src/Games/OneGame.tsx
+++ src/Games/OneGame.tsx
@@ -1,5 +1,6 @@
 import { GameEntity } from "../types";
 import { EditGame } from "./EditGame.tsx";
+import { useGetTeamsQuery } from "../queries/useGetTeamsQuery.ts";
 
 type OneGameProps = {
     game: GameEntity;
@@ -9,6 +10,14 @@ type OneGameProps = {
 
 export const OneGame = ({ game, isActive, toggleShowGameInfo }: OneGameProps) => {
 
+    const {data: teams, isFetching, error} = useGetTeamsQuery();
+
+    const teamA = teams?.find(team => team.id === game.teamAId);
+    const teamB = teams?.find(team => team.id === game.teamBId);
+
+    if (isFetching) return <p>Loading...</p>;
+    if (error) return <p>{error.message}</p>;
+
     return (<>
             <tr>
                 <td>{game.id}</td>
@@ -16,8 +25,8 @@ export const OneGame = ({ game, isActive, toggleShowGameInfo }: OneGameProps) =>
                 <td>{game.gameDate}</td>
                 <td>{game.venue}</td>
                 <td>{game.duration} min</td>
-                <td>{game.teamAId}</td>
-                <td>{game.teamBId}</td>
+                <td>{teamA?.teamName}</td>
+                <td>{teamB?.teamName}</td>
                 <td>{game.resultTeamA} : {game.resultTeamB}</td>
                 <td>
                     <button onClick={toggleShowGameInfo}>{isActive ? 'Cancel' : 'Edit Game'}</button>
